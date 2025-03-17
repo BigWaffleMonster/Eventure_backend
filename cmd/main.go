ceee1d62acaaff6ad50baa488d0e185600046e9b
Waffle Monster <wafflemonster@Waffles-MacBook-Air.local>
2025-03-17 19:59:12 +0300

    init

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 		return
 	}
-	// Подключение к PostgreSQL
+	// Подключение к PostgreSQL 11
 	db, err := utils.InitDB()
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
