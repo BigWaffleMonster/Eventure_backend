7074e1bab629872c5932972491684667a17def02
BigWaffleMonster <rachkov.daniil2001@gmail.com>
2026-02-16 00:55:08 +0300

    Initial commit (via bun create)

--- utils/validators/email_validator.go
+++
@@ -1,9 +0,0 @@
-package validators
-
-import "regexp"
-
-const emailValidationRegex = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
-
-func IsValidEmail(email string) bool {
-	return regexp.MustCompile(emailValidationRegex).MatchString(email)
-}
